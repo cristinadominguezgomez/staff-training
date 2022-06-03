4b125d1ac0e0029801e8ce4f2b3cf08796eb076b
cristina <dominguez.ac@gmail.com>
2022-06-03 12:35:46 +0200

    Created database

---
+++ database/initDB.js
@@ -0,0 +1,65 @@
+require("dotenv").config();
+const getPool = require("./getPool");
+
+const initDB = async () => {
+  try {
+    const pool = getPool();
+
+    console.log("Deleting tables...");
+
+    await pool.query("DROP TABLE IF EXISTS likes;");
+    await pool.query("DROP TABLE IF EXISTS exercises;");
+    await pool.query("DROP TABLE IF EXISTS employees;");
+
+    console.log("Creating employees table...");
+
+    await pool.query(`
+      CREATE TABLE employees (
+        id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        email VARCHAR(100) UNIQUE NOT NULL,
+        password VARCHAR(100) NOT NULL,
+        role ENUM("normal", "admin") DEFAULT "normal",
+        name VARCHAR(100),
+        avatar VARCHAR(200),
+        registrationCode VARCHAR(100),
+        created_at DATETIME DEFAULT CURRENT_TIMESTAMP)
+      `);
+
+    console.log("Creating exercises table...");
+
+    await pool.query(`
+        CREATE TABLE exercises (
+            id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+            title VARCHAR(100) NOT NULL,
+            description VARCHAR(500) NOT NULL,
+            image VARCHAR(100),
+            created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+            employeed_id INT UNSIGNED NOT NULL,
+            FOREIGN KEY (employeed_id) REFERENCES employees(id)
+            );
+            `);
+
+    console.log("Creating likes table...");
+
+    await pool.query(`
+    CREATE TABLE likes (
+        id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        description VARCHAR(500) NOT NULL,
+        image VARCHAR(100),
+        created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+        employee_id INT UNSIGNED NOT NULL,
+        FOREIGN KEY (employee_id) REFERENCES employees(id),
+        exercise_id INT UNSIGNED NOT NULL,
+        FOREIGN KEY (exercise_id) REFERENCES exercises(id)
+        );
+        `);
+
+    console.log("¡All done!");
+  } catch (error) {
+    console.error(error);
+  } finally {
+    process.exit();
+  }
+};
+
+initDB();
